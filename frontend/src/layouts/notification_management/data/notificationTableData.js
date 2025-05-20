2fd38ba17da52b6b0ace2fdbeb16fba36528934e
Hepha <trantrunghieudo@gmail.com>
2025-05-20 13:48:26 +0700

    Update BE web

--- frontend/src/layouts/notification_management/data/notificationTableData.js
+++ frontend/src/layouts/notification_management/data/notificationTableData.js
@@ -75,12 +75,6 @@ export default function data() {
   }, [selectedUsernames]);
 
   useEffect(() => {
-    if (editDialogOpen && editNotification.usernames instanceof Set) {
-      setSelectedUsernames(editNotification.usernames);
-    }
-  }, [editDialogOpen, editNotification.usernames]);
-
-  useEffect(() => {
     setEditNotification((prev) => ({
       ...prev,
       usernames: new Set(selectedUsernames),
@@ -584,7 +578,7 @@ export default function data() {
                 <MDInput
                   label="Selected Usernames"
                   name="usernames"
-                  value={[...newNotification.usernames].join(", ")}
+                  value={[...selectedUsernames].join(", ")}
                   disabled
                   fullWidth
                 />
