986f38d1bb74a403fb49071d75423d6bb0531fff
Hepha <trantrunghieudo@gmail.com>
2025-05-20 21:11:49 +0700

    Fix lỗi

--- frontend/src/layouts/contribution/data/userContributionData.js
+++ frontend/src/layouts/contribution/data/userContributionData.js
@@ -297,7 +297,16 @@ export default function userContributionData({ apartmentId }) {
       // apartmentId: bill.apartmentId,
       action: (
         <MDBox display="flex" gap={1}>
-          <MDButton variant="text" color="info" onClick={() => handleEditClick(bill)}>
+          <MDButton
+            variant="text"
+            color="info"
+            onClick={() => handleEditClick(bill)}
+            disabled={bill.status?.toLowerCase().trim() === "paid"}
+            sx={{
+              opacity: bill.status?.toLowerCase().trim() === "paid" ? 0.5 : 1,
+              pointerEvents: bill.status?.toLowerCase().trim() === "paid" ? "none" : "auto",
+            }}
+          >
             <Icon>edit</Icon>
           </MDButton>
           <MDButton variant="text" color="error" onClick={() => handleDeleteClick(bill)}>
